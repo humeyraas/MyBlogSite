5a41b45b41567cbc59e8d88dcd3e4878cc0cb5ad
beyzabaykal <149356794+beyzabaykal@users.noreply.github.com>
2025-06-12 18:12:57 +0300

    main ve beyza birleşti

--- MyBlogSite/Controllers/HomeController.cs
+++ MyBlogSite/Controllers/HomeController.cs
@@ -1,49 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyBlogSite.Data;
 using MyBlogSite.Models;
 using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
 
 namespace MyBlogSite.Controllers
 {
     public class HomeController : Controller
     {
-        
-        
         private readonly ApplicationDbContext _context;
 
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
-            ViewBag.Categories = _context.Categories.ToList();
-
         }
 
-        
-
-        public IActionResult Privacy()
-        {
-            ViewBag.Categories = _context.Categories.ToList();
-            return View();
-            
-
-        }
-        public IActionResult About()
-        {
-            ViewBag.Categories = _context.Categories.ToList();
-            return View();
-            
-
-
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            ViewBag.Categories = _context.Categories.ToList();
-
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
         public IActionResult Index(int? categoryId)
         {
             ViewBag.Categories = _context.Categories.ToList(); // Menü bar için
@@ -59,8 +30,55 @@ namespace MyBlogSite.Controllers
                 blogs = blogs.Where(b => b.CategoryId == categoryId.Value);
             }
 
+            // En çok görüntülenen 5 blog (Sidebar veya ana sayfa kýsmý için)
+            ViewBag.PopularBlogs = _context.Blogs
+                .OrderByDescending(b => b.Views)
+                .Take(5)
+                .ToList();
+
             return View(blogs.ToList());
         }
 
+        public IActionResult Search(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return RedirectToAction("Index");
+            }
+
+            var results = _context.Blogs
+                .Include(b => b.User)
+                .Include(b => b.Category)
+                .Where(b => b.Title.Contains(query) || b.Content.Contains(query))
+                .OrderByDescending(b => b.CreatedAt)
+                .ToList();
+
+            ViewBag.Categories = _context.Categories.ToList();
+            ViewBag.PopularBlogs = _context.Blogs
+                .OrderByDescending(b => b.Views)
+                .Take(5)
+                .ToList();
+
+            return View("SearchResults", results); // SearchResults.cshtml adýnda bir view gerektirir
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- MyBlogSite/Migrations/20250612133127_AddUserAboutAndProfileImage.cs
+++ MyBlogSite/Migrations/20250612133127_AddUserAboutAndProfileImage.cs
@@ -5,24 +5,18 @@
 namespace MyBlogSite.Migrations
 {
     /// <inheritdoc />
-    public partial class ApplyBeyzaChanges : Migration
+    public partial class AddUserAboutAndProfileImage : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "Tags",
-                table: "Blogs",
-                type: "nvarchar(max)",
-                nullable: true);
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Tags",
-                table: "Blogs");
+
         }
     }
 }
